c1f4c6c73ac6a0097689682eeca0b255adf302b4
Vasanth RS <vasanth.rs@proton.me>
2025-09-24 07:02:50 +0530

    03: authentication setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,71 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 export default function Home() {
-  return <Button variant="destructive">Click me</Button>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const {
+    data: session,
+    isPending, //loading state
+    error, //error object
+    refetch, //refetch the session
+  } = authClient.useSession();
+
+  const onSubmit = async () => {
+    const { data, error } = await authClient.signIn.email(
+      {
+        email,
+        password,
+      },
+      {
+        onRequest: (ctx) => {},
+        onSuccess: (ctx) => {
+          //redirect to the dashboard or sign in page
+          window.alert("Success");
+        },
+        onError: (ctx) => {
+          // display the error message
+          console.error(ctx.error);
+          window.alert(ctx.error.message);
+        },
+      }
+    );
+  };
+
+  if (session) {
+    return (
+      <div>
+        <p>{session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}>Sign Out</Button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      {/* <input
+        type="text"
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      /> */}
+      <input
+        type="text"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={onSubmit}>Create User</Button>
+    </div>
+  );
 }
