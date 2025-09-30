ebf15f811d29847dc5e7a4b1de1bd097312c9c46
Vasanth RS <vasanth.rs@proton.me>
2025-09-30 06:47:34 +0530

    08: trpc setup

--- src/modules/dashboard/ui/components/dashboard-navbar.tsx
+++ src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -40,8 +40,8 @@ export const DashboardNavbar = () => {
         >
           <SearchIcon />
           Search
-          <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium text-muted-foreground">
-            <span className="text-xs">&#8984;</span>K
+          <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center justify-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground leading-none">
+            <span className="text-xs leading-none">&#8984;</span>K
           </kbd>
         </Button>
       </nav>
